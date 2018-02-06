1cbacef1a2dfca99ad41a0f7d1bb2fbce6decf17
Stephanie Retzke <smrhfb@mail.umkc.edu>
2018-02-05 22:37:06 -0600

    Got the Language codes
    Language codes now work for the app

--- ICP4/TranslateApp/app/src/main/java/tutorial/cs5551/com/translateapp/TranslateActivity.java
+++ ICP4/TranslateApp/app/src/main/java/tutorial/cs5551/com/translateapp/TranslateActivity.java
@@ -64,12 +64,297 @@ public class TranslateActivity extends AppCompatActivity {
 
     public String getCode(String lang){
 
-        return "en";
-    }
-    public String getCode2(String lang){
+        String code = "en";
+        switch (lang){
+            case "Azerbaijan":
+                code = "az";
+                break;
+            case "Albanian":
+                code = "sq";
+                break;
+            case "Amharic":
+                code = "am";
+                break;
+            case "English":
+                code = "en";
+                break;
+            case "Arabic":
+                code = "ar";
+                break;
+            case "Armenian":
+                code = "hy";
+                break;
+            case "Afrikaans":
+                code = "af";
+                break;
+            case "Basque":
+                code = "eu";
+                break;
+            case "Bashkir":
+                code = "ba";
+                break;
+            case "Belarusian":
+                code = "be";
+                break;
+            case "Bengali":
+                code = "bn";
+                break;
+            case "Burmese":
+                code = "my";
+                break;
+            case "Bulgarian":
+                code = "bg";
+                break;
+            case "Bosnian":
+                code = "bs";
+                break;
+            case "Welsh":
+                code = "cy";
+                break;
+            case "Hungarian":
+                code = "hu";
+                break;
+            case "Vietnamese":
+                code = "vi";
+                break;
+            case "Haitian (Creole)":
+                code = "ht";
+                break;
+            case "Galician":
+                code = "gl";
+                break;
+            case "Dutch":
+                code = "nl";
+                break;
+            case "Hill Mari":
+                code = "mrj";
+                break;
+            case"Greek":
+                code = "el";
+                break;
+            case "Georgian":
+                code = "ka";
+                break;
+            case "Gujarati":
+                code = "gu";
+                break;
+            case "Danish":
+                code = "da";
+                break;
+            case "Hebrew":
+                code = "he";
+                break;
+            case "Yiddish":
+                code = "yi";
+                break;
+            case "Indonesian":
+                code = "id";
+                break;
+            case "Irish":
+                code = "ga";
+                break;
+            case "Italian":
+                code = "it";
+                break;
+            case "Icelandic":
+                code = "is";
+                break;
+            case "Spanish":
+                code = "es";
+                break;
+            case "Kazakh":
+                code = "kk";
+                break;
+            case "Kannada":
+                code = "kn";
+                break;
+            case "Catalan":
+                code = "ca";
+                break;
+            case "Kyrgyz":
+                code = "ky";
+                break;
+            case "Chinese":
+                code = "zh";
+                break;
+            case "Korean":
+                code = "ko";
+                break;
+            case "Xhosa":
+                code = "xh";
+                break;
+            case "Khmer":
+                code = "km";
+                break;
+            case "Laotian":
+                code = "lo";
+                break;
+            case "Latin":
+                code = "la";
+                break;
+            case "Latvian":
+                code = "lv";
+                break;
+            case "Lithuanian":
+                code = "lt";
+                break;
+            case "Luxembourgish":
+                code = "lb";
+                break;
+            case "Malagasy":
+                code = "mg";
+                break;
+            case "Malay":
+                code = "ms";
+                break;
+            case "Malayalam":
+                code = "ml";
+                break;
+            case "Maltese":
+                code = "mt";
+                break;
+            case "Macedonian":
+                code = "mk";
+                break;
+            case "Maori":
+                code = "mi";
+                break;
+            case "Marathi":
+                code = "mr";
+                break;
+            case "Mari":
+                code = "mhr";
+                break;
+            case "Mongolian":
+                code = "mn";
+                break;
+            case "German":
+                code = "de";
+                break;
+            case "Nepali":
+                code = "ne";
+                break;
+            case "Norwegian":
+                code = "no";
+                break;
+            case "Punjabi":
+                code = "pa";
+                break;
+            case "Papiamento":
+                code = "pap";
+                break;
+            case "Persian":
+                code = "fa";
+                break;
+            case "Polish":
+                code = "pl";
+                break;
+            case "Portuguese":
+                code = "pt";
+                break;
+            case "Romanian":
+                code = "ro";
+                break;
+            case "Russian":
+                code = "ru";
+                break;
+            case "Cebuano":
+                code = "ceb";
+                break;
+            case "Serbian":
+                code = "sr";
+                break;
+            case "Sinhala":
+                code = "si";
+                break;
+            case "Slovakian":
+                code = "sk";
+                break;
+            case "Slovenian":
+                code = "sl";
+                break;
+            case "Swahili":
+                code = "sw";
+                break;
+            case "Sudanese":
+                code = "su";
+                break;
+            case "Tajik":
+                code = "tg";
+                break;
+            case "Thai":
+                code = "th";
+                break;
+            case "Tagalog":
+                code = "tl";
+                break;
+            case "Tamil":
+                code = "ta";
+                break;
+            case "Tatar":
+                code = "tt";
+                break;
+            case "Telugu":
+                code = "te";
+                break;
+            case "Turkish":
+                code = "tr";
+                break;
+            case "Udmurt":
+                code = "udm";
+                break;
+            case "Uzbek":
+                code = "uz";
+                break;
+            case "Ukrainian":
+                code = "uk";
+                break;
+            case "Urdu":
+                code = "ur";
+                break;
+            case "Finnish":
+                code = "fi";
+                break;
+            case "French":
+                code = "fr";
+                break;
+            case "Hindi":
+                code = "hi";
+                break;
+            case "Croatian":
+                code = "hr";
+                break;
+            case "Czech":
+                code = "cs";
+                break;
+            case "Swedish":
+                code = "sv";
+                break;
+            case "Scottish":
+                code = "gd";
+                break;
+            case "Estonian":
+                code = "et";
+                break;
+            case "Esperanto":
+                code = "eo";
+                break;
+            case "Javanese":
+                code = "jv";
+                break;
+            case "Japanese":
+                code = "ja";
+                break;
 
-        return "es";
+            default:
+                code = "en";
+                break;
+        }
+
+
+        return code;
     }
+
 
     public void logout(View v) {
         Intent redirect = new Intent(TranslateActivity.this, LoginActivity.class);
@@ -85,7 +370,7 @@ public class TranslateActivity extends AppCompatActivity {
         String target = spinner2.getSelectedItem().toString();
 
         String src = getCode(source);
-        String trgt = getCode2(target);
+        String trgt = getCode(target);
 
         sourceText = sourceTextView.getText().toString();
         String getURL = "https://translate.yandex.net/api/v1.5/tr.json/translate?" +
